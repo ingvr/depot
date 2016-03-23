5ae061bc77dc36fa184d5858541a310944957e30
Ingvar <ingvar@nendo.ru>
2016-03-23 09:19:50 +0500

    14. Admin & sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
+	skip_before_action :authorize
+
 	def new
 	end
 
 	def create
 		user = User.find_by(name: params[:name])
-		if user and user.authentificate(params[:password])
+		if user and user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to admin_url
 		else
-			redirect_to login_url, alert "Wrong name and/or password"
+			redirect_to login_url, alert: "Wrong name and/or password"
 		end
 	end
 
