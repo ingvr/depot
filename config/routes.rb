f3c62331fa2f029e3f533491432bc0b8af02e131
Ingvar <ingvar@nendo.ru>
2016-03-25 12:21:29 +0500

    15. Add localization

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,18 @@ Depot::Application.routes.draw do
 
 	get "sessions/create"
 	get "sessions/destroy"
+
 	resources :users
 
-	resources :orders
-
-	resources :line_items
-
-	resources :carts
-
-	get "store/index"
 	resources :products do
 		get :who_bought, on: :member
+	end
+
+	scope '(:locale)' do
+		resources :orders
+		resources :line_items
+		resources :carts
+		root 'store#index', as: 'store', via: :all
 	end
 
 	# The priority is based upon order of creation: first created -> highest priority.
@@ -27,7 +28,7 @@ Depot::Application.routes.draw do
 	# You can have the root of your site routed with "root"
 	# root 'welcome#index'
 
-	root to: 'store#index', as: 'store'
+	# root to: 'store#index', as: 'store'
 
 	# Example of regular route:
 	#   get 'products/:id' => 'catalog#view'
